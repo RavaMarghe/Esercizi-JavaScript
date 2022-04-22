dd2ae4243ebe34d0a45107ca8419cdb5cb8b9005
Margherita Ravanello <margherita.ravanello@gmail.com>
2022-04-22 15:35:21 +0200

    feat: add ex-28

--- 28 - Optional chaining/exercise-2.js
+++ 28 - Optional chaining/exercise-2.js
@@ -1,5 +1,10 @@
 const order = {};
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
+/*if (order && order.customer && order.customer.address && !order.customer.address.city) {
+  console.log('City is required');
+}*/
+
+let city = order.customer?.address?.city;
+if (!city) {
   console.log('City is required');
 }
