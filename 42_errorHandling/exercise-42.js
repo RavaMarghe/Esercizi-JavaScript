a182a3521e84ff630b44ce27a56578509003bcee
Margherita Ravanello <margherita.ravanello@gmail.com>
2022-05-09 12:48:19 +0200

    feat: add ex-42

--- 42_errorHandling/exercise-42.js
+++ 42_errorHandling/exercise-42.js
@@ -7,19 +7,39 @@ class BankAccount {
 
   deposit(amount) {
     if (amount < 0) {
-      throw new Error('The amount provided cannot be negative');
+      try {
+        throw new Error('The amount provided cannot be negative');
+      }
+
+      catch (error) {
+        console.error(error.message);
+      }
     }
 
     this.#amount += amount;
   }
 
   withdraw(amount) {
+
     if (amount < 0) {
-      throw new Error('The amount provided cannot be negative');
+      try {
+        throw new Error('The amount provided cannot be negative');
+      }
+
+      catch (error) {
+        console.error(error.message);
+      }
     }
 
     if (this.#amount < amount) {
-      throw new Error('You cannot withdraw more than account balance');
+      try {
+        throw new Error('You cannot withdraw more than account balance');
+      }
+
+      catch (error) {
+        console.error(error.message);
+      }
+
     }
 
     this.#amount -= amount;
